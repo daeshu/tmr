4e6add522449bba8e3cefa73a69770a2dc5548ca
daeshu <daeshu@users.noreply.github.com>
2017-08-21 01:24:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,6 +31,39 @@
 
 
 
+// Send to Formspree
+$('.contact-form').on('submit', function(e) {
+		e.preventDefault();
+
+		//get the name field value
+		var name = $('#name').val();
+		//get the name field value
+		var email = $('#email').val();
+		//get the comments
+		var comments = $('#comments').val();
+
+		$.ajax({
+				url:'https://formspree.io/me@artemsheludko.pw',
+				method:'POST',
+				data:{
+						name:name,
+						_replyto:email,
+						comments:comments,
+						_subject:'Contact Form',
+				},
+				dataType:"json",
+				success:function() {
+						console.log('success');
+						$('.formBlock').hide();
+						$('.thankyouBlock').show();
+				}
+
+		});
+
+});
+
+});
+
 /*
 	PLUGINS
 */
