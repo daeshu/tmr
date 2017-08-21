af9819374e61a43632a9d9d000f54c8cd7245f6d
daeshu <daeshu@users.noreply.github.com>
2017-08-21 18:48:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,37 +26,6 @@
         });
 	}; // all loaded
 // Send to Formspree
-$('.contact-form').on('submit', function(e) {
-		e.preventDefault();
-
-		//get the name field value
-		var name = $('#name').val();
-		//get the name field value
-		var email = $('#email').val();
-		//get the comments
-		var comments = $('#comments').val();
-
-		$.ajax({
-				url:'https://formspree.io/me@artemsheludko.pw',
-				method:'POST',
-				data:{
-						name:name,
-						_replyto:email,
-						comments:comments,
-						_subject:'Contact Form',
-				},
-				dataType:"json",
-				success:function() {
-						console.log('success');
-						$('.formBlock').hide();
-						$('.thankyouBlock').show();
-				}
-
-		});
-
-});
-
-});
 //hasta aqui formspree
 	  
 }());
